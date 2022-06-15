f711132cc4cac201a6d375ac6e247508512a2518
yuanqing.li <yuanqing.li@goscn.io>
2022-06-15 23:05:01 +0800

    feat: int

--- .umirc.js
+++ .umirc.js
@@ -10,6 +10,10 @@ export default {
       path: '/',
       routes: [{ path: '/', component: '../pages/index' }],
     },
+    {
+      path: '/detail',
+      routes: [{ path: '/detail', component: '../pages/Detail' }],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
---
+++ src/pages/Detail/index.js
@@ -0,0 +1,16 @@
+import React, { useState, useMemo } from 'react';
+import { Button } from 'antd';
+import { router } from 'umi'
+
+import S from './index.less';
+
+const Detail = () => {
+
+  return (
+    <div className={S.wrapper}>
+Detail
+    </div>
+  );
+};
+
+export default Detail;
