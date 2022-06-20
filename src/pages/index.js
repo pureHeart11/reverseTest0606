eabf93b0777fb1d8f2ba98653d43ac4fb00eb949
李元庆 <liyuanq@weipaitang.com>
2022-06-20 16:46:26 +0800

    feat: init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import React, { useState, useMemo } from 'react';
+import React, { useEffect } from 'react';
+import { TpWatermark } from '@/utils/tools';
 import { router } from 'umi';
 
 import S from './index.less';
 
 const Index = () => {
   const imgData = ['well', '330', '380', '430', '480', '', '', ''];
+  useEffect(() => {
+    TpWatermark('苏州择智工业设计');
+  }, []);
   return (
     <div className={S.wrapper}>
-      <div className={S.title}>吴庄铁矿通风图主页</div>
+      <div className={S.title}>吴庄铁矿风试验模拟软件</div>
       <div className={S.list}>
         {imgData.map(item => (
           <div className={S.box}>
